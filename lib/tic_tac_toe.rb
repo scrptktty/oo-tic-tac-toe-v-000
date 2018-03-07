da6589480024c2af1568b12151a0837718c007eb
Kachel Wittig <kjwittig@gmail.com>
2018-03-07 06:01:28 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,7 +27,7 @@ def input_to_index(user_input)
 end
 
 def move(board, index, current_player="X")
-  board[index] = current_player
+  @board[index] = current_player
 end
 
 def position_taken?(board, location)
