567b4fc8162d8f07f69e2e36a12d47d28f0392de
Kachel Wittig <kjwittig@gmail.com>
2018-03-07 06:00:37 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,8 +26,16 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, current_player="X")
+def move(@board, index, current_player="X")
   board[index] = current_player
 end 
+
+def position_taken?(@board, location)
+  @board[location] != " " && @board[location] != ""
+end
+
+def valid_move?(@board, index)
+  index.between?(0,8) && !position_taken?(@board, index)
+end
 
 end
